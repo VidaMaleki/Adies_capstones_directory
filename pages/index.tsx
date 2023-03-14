fe1682626f1cbfe4d0c37139505f9c67c80901d6
Megan Korling <korlingm@gmail.com>
2023-03-14 10:48:04 -0700

    Basic navbar set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Navbar />
         <h1>Adies Capstones Hub Main Page</h1>
         <p>View all apps here</p>
       </main>
