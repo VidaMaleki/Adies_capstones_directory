24274d25c4bbf809121451b61427e301ae42be88
Megan Korling <korlingm@gmail.com>
2023-03-14 11:11:53 -0700

    Navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,13 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/sign-in">Sign In</Link>
-                </li>
-            </ul>
+        <div className="navbar-container">
+            <div>
+                <Link href="/" className="nav-link">Home</Link>
+            </div>
+            <div>
+                <Link href="/sign-in" className="nav-link">Sign In</Link>
+            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import styles from '@/styles/Home.module.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
