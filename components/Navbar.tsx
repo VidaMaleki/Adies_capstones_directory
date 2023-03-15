620ccf5613dfd50b65f5e41748fc53329cc14159
Vida Maleki <vida.basmani@gmail.com>
2023-03-15 10:47:49 -0700

    Added style to navbar and added logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import LOGO from '../images/LOGO.png';
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <div className="navbar-container">
+            <Image id="nav-img" src={LOGO} alt="Image" />
             <div>
-                <Link href="/" className="nav-link">Home</Link>
+                <Link href="/" id="nav-link">Home</Link>
             </div>
             <div>
-                <Link href="/sign-in" className="nav-link">Sign In</Link>
+                <Link href="/sign-in" id="nav-link">Sign In</Link>
             </div>
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-// import '@/styles/globals.css'
+import '@/styles/globals.css'
 import '@/styles/navbar.css'
 import type { AppProps } from 'next/app'
 import { BrowserRouter } from 'react-router-dom'
