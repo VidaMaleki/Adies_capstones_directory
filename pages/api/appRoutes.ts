1d0d2c6ceb8d0c866546347df9b5309da0875636
Vida Maleki <vida.basmani@gmail.com>
2023-04-03 10:46:29 -0700

    Created a post request in app routes

---
+++ pages/api/appRoutes.ts
@@ -0,0 +1,61 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Developers, Apps} from '@prisma/client'
+import { db } from '@/lib/db';
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const { name } = req.body;
+    res.status(200).json({ message: `Hello, ${name}!` });
+  } else {
+    res.status(400).json({ message: 'Method not allowed.' });
+  }
+}
+
+const createApp = async ( req: NextApiRequest, res: NextApiResponse) => {
+  
+  const id: number = req.body.id;
+  const appName : string = req.body.appName;
+  const description: string = req.body.description;
+  const developer: Developers[] = req.body.developer;
+  const appLink: string = req.body.appLink;
+  const videoLink: string = req.body.videoLink;
+  const github: string = req.body.github;
+  const type: string = req.body.type;
+  const technologies: string = req.body.technologies;
+
+  await db.apps.create({data: {id ,  appName, description, developer, appLink, videoLink,github,type, technologies}})
+}
+
+
+
+
+
+
+// model Developers {
+//   id      Int      @id @default(autoincrement())
+//   name    String
+//   email   String   @unique
+//   linkedin String?
+//   app       Apps     @relation(fields: [app_id], references: [id])
+//   app_id    Int      @unique
+// }
+
+// model Apps {
+//   id      Int      @id @default(autoincrement())
+//   appName   String
+//   description String
+//   developer  Developers[]
+//   appLink String?
+//   videoLink String?
+//   github String?
+//   type String
+//   technologies String[]
+// }
