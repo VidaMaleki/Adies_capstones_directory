1e50b50bf044dca398597ee435fb08eb7d59a9e2
Megan Korling <korlingm@gmail.com>
2023-05-21 15:31:07 -0700

    Add background color, Refactor

--- pages/capstone.tsx
+++ pages/capstone.tsx
@@ -74,6 +74,7 @@ export default function Capstone ({ allDevs, signedInUser }: {
         { value: "game", label: "Game" },
         { value: "social", label: "Social" }
     ];
+    const nameOptions = allDevs.map(name => ({ value: String(name.id), label: name.fullName }));
 
     const handleSubmit = async (event: any) => {
         event.preventDefault();
@@ -93,96 +94,73 @@ export default function Capstone ({ allDevs, signedInUser }: {
     }
 
     const handleChange = (event: any) => {
-        // console.log(event);
         let newAppData: AppProperties = { ...appData };
-        // this is why value prop isn't needed
         if (event.label) {
-            // type has one option, so it returns {value: "", label: ""}
+            // event for app type has one option, so it returns {value: "", label: ""}
             // https://stackoverflow.com/questions/71934348/react-event-target-is-undefined-when-trying-to-filter-react-select-options
             newAppData.type = event.value;
         } else {
-            const inputName: string = event.target.name;
+            // all non-array values for app
+            const inputName = event.target.name;
             const targetValue = event.target.value;
-            // newAppData[inputName as keyof AppProperties] = targetValue;
             newAppData[inputName as keyof AppProperties] = targetValue;
         } 
         setAppData(newAppData);
-        // console.log(appData);
     }
-
-    const convertDeveloperNames = (names: Developer[]) => {
-        const nameOptions = [];
-        for (let dev of names) {
-            const newLabel = dev.fullName;
-            // const newValue = dev.fullName.toLowerCase().replace(/\s/g, '');
-            // names might not be unique, so use id as string as value
-            nameOptions.push({ value: String(dev.id), label: newLabel });
-        }
-        return nameOptions
-    }
-    const nameOptions = convertDeveloperNames(allDevs);
 
     const handleTechnologiesChange = (event: any) => {
-        // console.log(event);
         let newAppData = { ...appData };
-        // technologies has more than one option, returns a list [{value: "", label: ""}, ...]
-        const currTechs = [];
-        for (let option of event) {
-            currTechs.push(option.label);
-        }
+        const currTechs = event.map((option: { label: any; }) => option.label);
         newAppData.technologies = currTechs;
         setAppData(newAppData);
     }
 
     const handleDevChange = (event: any) => {
         let newAppData = { ...appData };
-        const currDevs: string[] = [];
-        for (let option of event) {
-            console.log(typeof option.value)
-            currDevs.push(option.value);
-        }
+        const currDevs: string[] = event.map((option: { value: any; }) => option.value);
         newAppData.developers = currDevs;
         setAppData(newAppData);
     }
+
     return (
-    <div className={styles.appCardWrapper}>
-        <h2>Capstone app</h2>
-        <form onSubmit={handleSubmit}>
-                <label>
-                    App Name
-                    <input type="text" value={appData.appName} onChange={handleChange} name="appName" />
-                </label>
-                <label>
-                    Description
-                    <textarea value={appData.description} onChange={handleChange} name="description" />
-                </label>
-                <label>
-                    Github Link
-                    <input type="text" value={appData.github} onChange={handleChange} name="github"/>
-                </label>
-                <label>
-                    App Link
-                    <input type="text" value={appData.appLink} onChange={handleChange} name="appLink"/>
-                </label>
-                <label>
-                    Video Demo Link
-                    <input type="text" value={appData.videoLink} onChange={handleChange} name="videoLink"/>
-                </label>
-                <label>
-                    Category
-                    <Select options={typeOptions} onChange={handleChange} instanceId="appType" />
-                </label>
-                <label>
-                    Technologies
-                    <CreatableSelect options={techOptions} onChange={handleTechnologiesChange} isMulti isClearable instanceId="appTechnologies" />
-                </label>
-                <label>
-                    Developers
-                    <CreatableSelect options={nameOptions} onChange={handleDevChange} isMulti isClearable instanceId="appDevs" />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-    </div>
+        <div className={styles.appCardWrapper}>
+            <h2>Your Capstone App</h2>
+            <form onSubmit={handleSubmit}>
+                    <label>
+                        App Name
+                        <input type="text" value={appData.appName} onChange={handleChange} name="appName" />
+                    </label>
+                    <label>
+                        Description
+                        <textarea value={appData.description} onChange={handleChange} name="description" />
+                    </label>
+                    <label>
+                        Github Link
+                        <input type="text" value={appData.github} onChange={handleChange} name="github"/>
+                    </label>
+                    <label>
+                        App Link
+                        <input type="text" value={appData.appLink} onChange={handleChange} name="appLink"/>
+                    </label>
+                    <label>
+                        Video Demo Link
+                        <input type="text" value={appData.videoLink} onChange={handleChange} name="videoLink"/>
+                    </label>
+                    <label>
+                        Category
+                        <Select options={typeOptions} onChange={handleChange} instanceId="appType" />
+                    </label>
+                    <label>
+                        Technologies
+                        <CreatableSelect options={techOptions} onChange={handleTechnologiesChange} isMulti isClearable instanceId="appTechnologies" />
+                    </label>
+                    <label>
+                        Developers
+                        <CreatableSelect options={nameOptions} onChange={handleDevChange} isMulti isClearable instanceId="appDevs" />
+                    </label>
+                    <input type="submit" value="Submit" className={styles.submitButton}/>
+                </form>
+        </div>
     )     
 }
 
