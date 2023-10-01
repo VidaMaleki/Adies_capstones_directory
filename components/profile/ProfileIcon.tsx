87096e25c7381822a807f29d243e4dd5e34dba8d
Vida Maleki <vida.basmani@gmail.com>
2023-09-30 22:52:53 -0700

    Fixed profile image to show image change immediately

--- components/profile/ProfileIcon.tsx
+++ components/profile/ProfileIcon.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import styles from "@/styles/Profile.module.css";
+import axios from "axios";
+import { DeveloperProps } from '../types';
+import { Developer } from "@prisma/client";
 
 interface AccountStatusResponse {
   isDeleted: boolean;
@@ -11,6 +14,23 @@ interface AccountStatusResponse {
 const ProfileIcon = (): JSX.Element | null => {
   const { data: session } = useSession();
   const [isDeleted, setIsDeleted] = useState(false);
+  const [developer, setDeveloper] = useState<DeveloperProps | null>(null);
+
+  const getDeveloper = async () => {
+    try {
+      const response = await axios.get("/api/auth/signup");
+      const developers: DeveloperProps[] = response.data; // Assuming developers are returned as an array
+      const matchingDeveloper = developers.find(developer => developer.email === session?.user?.email);
+      
+      if (matchingDeveloper) {
+        setDeveloper(matchingDeveloper);
+      } else {
+        console.error("Developer not found for the current user.");
+      }
+    } catch (error) {
+      console.error("Error fetching developer data: ", error);
+    }
+  };
 
   useEffect(() => {
     const fetchAccountStatus = async () => {
@@ -26,17 +46,20 @@ const ProfileIcon = (): JSX.Element | null => {
     };
 
     fetchAccountStatus();
+    if (session) {
+      getDeveloper();
+    }
   }, [session]);
 
   if (!session || isDeleted) {
     return null;
   }
-
+  console.log("Developer", developer)
   return (
     <Link href="/profile">
       <div className={styles.imageWrapper}>
         <Image
-          src={`/profile-pictures/${session?.user?.image!}.png`}
+          src={`/profile-pictures/${developer?.image}.png`}
           alt="Developer profile image"
           width={40}
           height={40}
