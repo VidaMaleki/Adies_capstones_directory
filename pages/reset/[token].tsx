28f7dfcd6396074c2a6a8da6dff01a2fef659395
Andrea Garcia <andrygzt@gmail.com>
2023-10-07 03:48:01 -0700

    reset page is now using same style as the rest of pages

--- pages/reset/[token].tsx
+++ pages/reset/[token].tsx
@@ -1,20 +1,28 @@
+import Navbar from "@/components/Navbar/Navbar";
 import Background from "@/components/SignIn/components/background/Background";
 import ResetForm from "@/components/SignIn/components/forms/Reset";
 import { NextPageContext } from "next";
+import styles from "@/styles/auth.module.css";
 // npm install --save react-toastify
 
 export default function auth({ token }: { token: string }) {
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="w-full h-100 flex items-center justify-center">
-        {/*---Form---- */}
-        <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex items-center justify-center">
-          <ResetForm token={token} />
+    <div className={styles.authWrapper}>
+      <Navbar />
+      <div className={styles.authContainer}>
+        <div className="w-full flex items-center justify-center">
+          <div className="w-full h-100 flex items-center justify-center">
+            {/*---Form---- */}
+            <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex items-center justify-center">
+              <ResetForm token={token} />
+            </div>
+            {/*---Background---*/}
+            <Background image={`"../auth/reset.png"`} />
+          </div>
         </div>
-        {/*---Background---*/}
-        <Background image={`"../auth/reset.png"`} />
       </div>
     </div>
+
   );
 }
 
