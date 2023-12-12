abc2644ed3626c98034dd12cd61aa21a70582b57
Vida Maleki <vida.basmani@gmail.com>
2023-12-11 19:40:16 -0800

    Added session cookie extentions to next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com", "www.paypal.com"],
-  },
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ["res.cloudinary.com", "www.paypal.com"],
+    },
+    webpack: (config) => {
+        config.experiments = {...config.experiments, topLevelAwait: true };
+        return config;
+    },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+    },
+    session: {
+        jwt: true,
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
 };
 
 module.exports = nextConfig;
