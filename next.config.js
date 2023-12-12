45a54df785b19d2682c486b2a809964164d9bb6c
Vida Maleki <vida.basmani@gmail.com>
2023-12-11 20:15:06 -0800

    Modified next.config file

--- next.config.js
+++ next.config.js
@@ -8,13 +8,6 @@ const nextConfig = {
         config.experiments = {...config.experiments, topLevelAwait: true };
         return config;
     },
-    env: {
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-    },
-    session: {
-        jwt: true,
-        maxAge: 30 * 24 * 60 * 60, // 30 days
-    },
 };
 
 module.exports = nextConfig;
